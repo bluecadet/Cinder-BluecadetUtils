d4b92a664f81f9f18217b6cf602588339e15519b
Benjamin Bojko <ben@bluecadet.com>
2016-07-20 16:12:32 -0400

    Added example button

--- samples/AppSample/src/AppSampleApp.cpp
+++ samples/AppSample/src/AppSampleApp.cpp
@@ -3,22 +3,33 @@
 #include "cinder/gl/gl.h"
 
 #include "BaseApp.h"
+#include "debug/RectButton.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 using namespace bluecadet::utils;
+using namespace bluecadet::views;
 
 class AppSampleApp : public BaseApp {
 public:
 	void setup() override;
 	void update() override;
 	void draw() override;
+
+protected:
+	debug::RectButtonRef mButton;
 };
 
 void AppSampleApp::setup()
 {
 	BaseApp::setup();
+
+	mButton = bluecadet::views::debug::RectButtonRef(new bluecadet::views::debug::RectButton());
+	mButton->setup(vec2(100.0f, 100.0f));
+	mButton->setPosition((vec2(getWindowSize()) - mButton->getSize()) * 0.5f);
+
+	mRootView->addChild(mButton);
 }
 
 void AppSampleApp::update()
