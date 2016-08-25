86ba08ddf70e0954f0bddaf513360a53151e9b09
Benjamin Bojko <ben@bluecadet.com>
2016-08-25 10:56:56 -0400

    Added support to parse CLI args; Added hasField(); Added clearColor

--- src/SettingsManager.cpp
+++ src/SettingsManager.cpp
@@ -17,13 +17,12 @@ SettingsManager::SettingsManager() {
 	mConsoleWindowEnabled = true;
 	mFps = 60;
 	mAppVersion = "";
-	mAppWidth = 1920;
-	mAppHeight = 1080;
 
 	// Graphics
 	mVerticalSync = true;
+	mClearColor = ci::ColorA(0, 0, 0, 1.0f);
 
-	// Debug
+	// Debugging
 	mDebugMode = true;
 	mDrawMinimap = true;
 	mDebugDrawTouches = false;
@@ -43,6 +42,9 @@ SettingsManager::~SettingsManager() {}
 // Pull in the shared/standard app settings JSON
 void SettingsManager::setup(const ci::fs::path& jsonPath, ci::app::App::Settings* appSettings) {
 
+	// default window size to main display size
+	mDebugWindowSize = Display::getMainDisplay()->getSize();
+
 	// If the path exists, load it
 	if (fs::exists(jsonPath)) {
 		console() << "SettingsManager: Loading settings from '" << jsonPath << "'" << endl;
@@ -52,26 +54,26 @@ void SettingsManager::setup(const ci::fs::path& jsonPath, ci::app::App::Settings
 			mAppSettingsDoc = (JsonTree)tree;
 
 			// General
-			updatePropertyFromField(&mConsoleWindowEnabled, "settings.general.consoleWindowEnabled");
-			updatePropertyFromField(&mFps, "settings.general.FPS");
-			updatePropertyFromField(&mAppVersion, "settings.general.appVersion");
+			setFieldFromJsonIfExists(&mConsoleWindowEnabled, "settings.general.consoleWindowEnabled");
+			setFieldFromJsonIfExists(&mFps, "settings.general.FPS");
+			setFieldFromJsonIfExists(&mAppVersion, "settings.general.appVersion");
 
 			// Graphics
-			updatePropertyFromField(&mVerticalSync, "settings.graphics.verticalSync");
+			setFieldFromJsonIfExists(&mVerticalSync, "settings.graphics.verticalSync");
 
 			// Debug
-			updatePropertyFromField(&mDebugMode, "settings.debug.debugMode");
-			updatePropertyFromField(&mDrawMinimap, "settings.debug.drawMinimap");
-			updatePropertyFromField(&mDebugDrawTouches, "settings.debug.drawTouches");
-			updatePropertyFromField(&mDebugDrawScreenLayout, "settings.debug.drawScreenLayout");
-			updatePropertyFromField(&mDebugFullscreen, "settings.debug.fullscreen");
-			updatePropertyFromField(&mDebugBorderless, "settings.debug.borderless");
-			updatePropertyFromField(&mShowMouse, "settings.debug.showMouse");
+			setFieldFromJsonIfExists(&mDebugMode, "settings.debug.debugMode");
+			setFieldFromJsonIfExists(&mDrawMinimap, "settings.debug.drawMinimap");
+			setFieldFromJsonIfExists(&mDebugDrawTouches, "settings.debug.drawTouches");
+			setFieldFromJsonIfExists(&mDebugDrawScreenLayout, "settings.debug.drawScreenLayout");
+			setFieldFromJsonIfExists(&mDebugFullscreen, "settings.debug.fullscreen");
+			setFieldFromJsonIfExists(&mDebugBorderless, "settings.debug.borderless");
+			setFieldFromJsonIfExists(&mShowMouse, "settings.debug.showMouse");
 
 			// Analytics
-			updatePropertyFromField(&mAnalyticsAppName, "settings.analytics.appName");
-			updatePropertyFromField(&mAnalyticsTrackingId, "settings.analytics.trackingID");
-			updatePropertyFromField(&mAnalyticsClientId, "settings.analytics.clientID");
+			setFieldFromJsonIfExists(&mAnalyticsAppName, "settings.analytics.appName");
+			setFieldFromJsonIfExists(&mAnalyticsTrackingId, "settings.analytics.trackingID");
+			setFieldFromJsonIfExists(&mAnalyticsClientId, "settings.analytics.clientID");
 
 		} catch (Exception e) {
 			console() << "SettingsManager: ERROR: Could not load settings json: " << e.what() << endl;
@@ -81,13 +83,23 @@ void SettingsManager::setup(const ci::fs::path& jsonPath, ci::app::App::Settings
 	}
 
 	// Parse arguments from command line
-	parseCommandLineArgs(appSettings->getCommandLineArgs());
+	addCommandLineParser("debug", [&](const string &value) { mDebugMode = value == "true"; });
+	addCommandLineParser("fullscreen", [&](const string &value) { mDebugFullscreen = value == "true"; });
+	addCommandLineParser("borderless", [&](const string &value) { mDebugBorderless = value == "true"; });
+	addCommandLineParser("vsync", [&](const string &value) { mVerticalSync = value == "true"; });
+	addCommandLineParser("console", [&](const string &value) { mConsoleWindowEnabled = value == "true"; });
+	addCommandLineParser("cursor", [&](const string &value) { mShowMouse = value == "true"; });
+	addCommandLineParser("mouse", [&](const string &value) { mShowMouse = value == "true"; });
+	addCommandLineParser("size", [&](const string &value) {
+		int commaIndex = (int)value.find(",");
+		if (commaIndex != string::npos) {
+			string wStr = value.substr(0, commaIndex);
+			string hStr = value.substr(commaIndex + 1, value.size() - commaIndex - 1);
+			mDebugWindowSize = ivec2(stoi(wStr), stoi(hStr));
+		}
+	});
 
 	// Apply settings
-	applySettings(appSettings);
-}
-
-void SettingsManager::applySettings(ci::app::App::Settings* appSettings) {
 	appSettings->setConsoleWindowEnabled(mConsoleWindowEnabled);
 	appSettings->setFrameRate((float)mFps);
 
@@ -99,67 +111,33 @@ void SettingsManager::applySettings(ci::app::App::Settings* appSettings) {
 	appSettings->setFullScreen(mDebugFullscreen);
 }
 
-void SettingsManager::parseArgumentsMap(const std::map<std::string, std::string>& argsMap) {
+void SettingsManager::addCommandLineParser(const std::string &key, CommandLineArgParserFn callback)
+{
+	auto callbackListIt = mCommandLineArgsHandlers.find(key);
 
-	// default window size to main display size
-	mDebugWindowSize = Display::getMainDisplay()->getSize();
-
-	// process command line args
-	for (std::pair<std::string, std::string> arg : argsMap) {
-
-		if (arg.first == "debug") {
-			mDebugMode = arg.second == "true";
-		}
-
-		else if (arg.first == "size") {
-			int commaIndex = (int)arg.second.find(",");
-			if (commaIndex != string::npos) {
-				string wStr = arg.second.substr(0, commaIndex);
-				string hStr = arg.second.substr(commaIndex + 1, arg.second.size() - commaIndex - 1);
-				mDebugWindowSize = ivec2(stoi(wStr), stoi(hStr));
-			}
-		}
-
-		else if (arg.first == "fullscreen") {
-			mDebugFullscreen = arg.second == "true";
-		}
-
-		else if (arg.first == "borderless") {
-			mDebugBorderless = arg.second == "true";
-		}
-
-		else if (arg.first == "vsync") {
-			mVerticalSync = arg.second == "true";
-		}
-
-		else if (arg.first == "console") {
-			mConsoleWindowEnabled = arg.second == "true";
-		}
-
-		else if (arg.first == "cursor" || arg.first == "mouse") {
-			mShowMouse = arg.second == "true";
-		}
+	if (callbackListIt == mCommandLineArgsHandlers.end()) {
+		mCommandLineArgsHandlers[key] = vector<CommandLineArgParserFn>();
+		callbackListIt = mCommandLineArgsHandlers.find(key);
 	}
+
+	callbackListIt->second.push_back(callback);
 }
 
 void SettingsManager::parseCommandLineArgs(const std::vector<std::string>& args) {
-	// Process command line args
-	if (!args.empty()) {
+	for (auto arg : args) {
+		int splitIndex = (int)arg.find_first_of("=");
+		if (splitIndex != std::string::npos) {
+			std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
+			std::string key = arg.substr(0, splitIndex);
+			std::string value = arg.substr(splitIndex + 1, arg.size() - splitIndex - 1);
 
-		std::map<std::string, std::string> argsMap;
+			auto callbackListIt = mCommandLineArgsHandlers.find(key);
+			if (callbackListIt == mCommandLineArgsHandlers.end()) continue;
 
-		for (const std::string carg : args) {
-			std::string arg = carg;
-			int splitIndex = (int)arg.find_first_of("=");
-			if (splitIndex != std::string::npos) {
-				std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
-				std::string key = arg.substr(0, splitIndex);
-				std::string value = arg.substr(splitIndex + 1, arg.size() - splitIndex - 1);
-				argsMap[key] = value;
+			for (auto callback : callbackListIt->second) {
+				callback(value);
 			}
 		}
-
-		parseArgumentsMap(argsMap);
 	}
 }
 
@@ -167,7 +145,7 @@ ci::params::InterfaceGlRef SettingsManager::getParams() {
 	static ci::params::InterfaceGlRef params = nullptr;
 	if (!params) {
 		params = ci::params::InterfaceGl::create("Settings", ci::ivec2(250, 500));
-		params->addParam("Minimap", &mDrawMinimap);
+		params->addParam("Show Minimap", &mDrawMinimap);
 		params->addParam("Show Touches", &mDebugDrawTouches);
 		params->addParam("Show Layout", &mDebugDrawScreenLayout);
 		params->addParam("Show Cursor", &mShowMouse).updateFn([&] { mShowMouse ? ci::app::AppBase::get()->showCursor() : ci::app::AppBase::get()->hideCursor(); }).key("m");
