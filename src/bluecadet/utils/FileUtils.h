04cb5eea63218eb171935c5a18b07325b5c0dd2c
kevinzak <kevinJzakszewski@gmail.com>
2021-10-12 11:57:03 -0400

    Converting ci to cinder namespace

--- src/bluecadet/utils/FileUtils.h
+++ src/bluecadet/utils/FileUtils.h
@@ -12,7 +12,7 @@ class FileUtils {
 
 public:
 	
-	typedef std::function<void(const ci::fs::path & path)> FileCallback;
+	typedef std::function<void(const cinder::fs::path & path)> FileCallback;
 
 	/// <summary>
 	/// Finds all files in a directory and triggers callback on each depth-first.
@@ -22,13 +22,13 @@ public:
 	/// <param name="extensions">Valid extensions to load, e.g. {".json", ".csv"}. If lowercaseExtensions is true, all extensions should be lowercase only.</param>
 	/// <param name="recursive">Will load all subdirectories too if set to true.</param>
 	/// <param name="lowercaseExtensions">If true, will convert all found extensions to lowercase. This presumes that all extenssions in the extensions parameter are also lowercase.</param>
-	static void find(const ci::fs::path & dir, FileCallback callback, const std::set<std::string> extensions = {}, const bool recursive = true, const bool lowercaseExtensions = true);
+	static void find(const cinder::fs::path & dir, FileCallback callback, const std::set<std::string> extensions = {}, const bool recursive = true, const bool lowercaseExtensions = true);
 
 	//! Filename including extension. Avoids issues with compiler/IDE version discrepancies.
-	static std::string getFilename(const ci::fs::path & path);
+	static std::string getFilename(const cinder::fs::path & path);
 
 	//! Extension starting at the last '.' (e.g. 'file.ext' will return '.ext'). Avoids issues with compiler/IDE version discrepancies.
-	static std::string getExtension(const ci::fs::path & path);
+	static std::string getExtension(const cinder::fs::path & path);
 };
 
 }
